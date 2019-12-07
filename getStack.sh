1213ad0f338459eb2bd99a81d0cd416f82cae86f
Ben Hart <benjmhart@gmail.com>
2019-12-07 10:29:36 -0500

    Feature/ci (#10)
    * pull ormolu out of ci
    
    * circleci adjustment
    
    * circleci adjustment
    
    * use wget to install hlint
    
    * nuke and rebuild cabal
    
    * bump stack version
    
    * bump stack version
    
    * move stack to a seperate script to reduce repetitive code,  shuffle where linting happens
    
    * diagnosing issue with sudo
    
    * diagnosing issue with sudo
    
    * diagnosing issue with sudo
    
    * fix broken url
    
    * fix broken url

---
+++ getStack.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+version="2.1.1"
+
+wget https://github.com/commercialhaskell/stack/releases/download/v$version/stack-$version-linux-x86_64.tar.gz -O /tmp/stack.tar.gz
+
+sudo mkdir /tmp/stack-download
+
+sudo tar -xzf /tmp/stack.tar.gz -C /tmp/stack-download
+
+sudo chmod +x /tmp/stack-download/stack-$version-linux-x86_64/stack
+
+sudo mv /tmp/stack-download/stack-$version-linux-x86_64/stack /usr/bin/stack
+
+
