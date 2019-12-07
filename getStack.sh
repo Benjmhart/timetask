22cdf5c8e4417191eefbf80b34dad59b33a51b11
benjmhart <benjmhart@gmail.com>
2019-12-07 00:04:31 -0500

    fix broken url

--- getStack.sh
+++ getStack.sh
@@ -3,8 +3,13 @@
 version="2.1.1"
 
 wget https://github.com/commercialhaskell/stack/releases/download/v$version/stack-$version-linux-x86_64.tar.gz -O /tmp/stack.tar.gz
+
 sudo mkdir /tmp/stack-download
+
 sudo tar -xzf /tmp/stack.tar.gz -C /tmp/stack-download
+
 sudo chmod +x /tmp/stack-download/stack-$version-linux-x86_64/stack
-run: sudo mv /tmp/stack-download/stack-$version-linux-x86_64/stack /usr/bin/stack
+
+sudo mv /tmp/stack-download/stack-$version-linux-x86_64/stack /usr/bin/stack
+
 
